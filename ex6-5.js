e3fbf1318a5dd4a34d79824ea9bc003fbca1104c
k-kota <zm18210@josai.ac.jp>
2020-10-20 01:52:12 +0000

    message

--- ex6-5.js
+++ ex6-5.js
@@ -1,2 +1,3 @@
-varwho = 'world';function sayhello() {alert('Hello, ' + who + '!');}
+function sayhello() {varwho = 'world';alert('Hello, ' + who + '!');}
 function taro() {who = '太郎';}
+function hanako() {who = '花子';}
