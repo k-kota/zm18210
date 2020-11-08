4c5e5c9d7a0d3583256d1cf330277f6952e73aea
k-kota <zm18210@josai.ac.jp>
2020-11-08 15:18:32 +0000

    message

--- ex6-5.js
+++ ex6-5.js
@@ -1,7 +1,20 @@
-var　who65='world';
-function sayhello() {alert('Hello,'+ who65+'!');}
-function taro65() {who='太郎';}
-function hanako65() {who='花子';}
+var　who65 = 'world';
 
-function taro65() {alert('太郎'+'が来た'+'!');}
-function hanako65() {alert('花子'+'が来た'+'!');}
+function sayhello() {
+    alert('Hello, ' + who65 + '!');
+}
+
+function taro65() {
+    who65 = '太郎' ;
+}
+function hanako65() {
+    who65 = '花子' ;
+}
+
+function gakita() {
+    alert('太郎, ' + が来た + '!');
+}
+
+function gakita() {
+    alert('花子, ' + が来た + '!');
+}
