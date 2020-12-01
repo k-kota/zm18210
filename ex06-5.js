4da358c089fe45774c078c61d21dd2550916f1d0
k-kota <zm18210@josai.ac.jp>
2020-12-01 01:29:35 +0000

    message

--- ex06-5.js
+++ ex06-5.js
@@ -4,12 +4,12 @@ function sayhello65() {
   alert('Hello, ' + who65 + '!');
 }
 
-function taro65(){
+function taro65() {
   who65 = '太郎'　;
   alert(who65 +'が来た');
 }
 
 function hanako65() {
-  who65 = '花子'　;
+  who65 = '花子'　;  
   alert(who65 +'が来た');
 }
