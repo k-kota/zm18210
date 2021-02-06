d18b9566a4e13b58dd15a379c3e1d38073d8d1be
k-kota <zm18210@josai.ac.jp>
2021-02-06 13:40:10 +0000

    message

--- ex06-9.js
+++ ex06-9.js
@@ -1,6 +1,7 @@
 function txtchange1() {
-document.getElementById('txt1').innerHTML= 'ナンバーワン！';
+  document.getElementById('txt1').innerHTML = 'ナンバーワン！';
 }
+
 function imgchange1() {
   document.getElementById('logo').src = './image/man-u.png';
 }
